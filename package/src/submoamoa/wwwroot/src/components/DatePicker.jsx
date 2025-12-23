80396a1da3cd4177910d04c2fc4841ffb2de621d
Jaromir Sivic <sivicjaromir@gmail.com>
2025-12-24 00:23:18 +0100

    Update of the DatePicker

--- package/src/submoamoa/wwwroot/src/components/DatePicker.jsx
+++ package/src/submoamoa/wwwroot/src/components/DatePicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
+import calendarIcon from '../assets/icons/calendar.svg';
 
 /**
  * DatePicker component for selecting dates with calendar popup.
@@ -371,21 +372,7 @@ const DatePicker = ({
                 }}
             >
                 {/* Calendar icon */}
-                <svg 
-                    width="20" 
-                    height="20" 
-                    viewBox="0 0 24 24" 
-                    fill="none" 
-                    stroke="#666" 
-                    strokeWidth="2"
-                    strokeLinecap="round" 
-                    strokeLinejoin="round"
-                >
-                    <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
-                    <line x1="16" y1="2" x2="16" y2="6" />
-                    <line x1="8" y1="2" x2="8" y2="6" />
-                    <line x1="3" y1="10" x2="21" y2="10" />
-                </svg>
+                <img src={calendarIcon} alt="" width="20" height="20" style={{ opacity: 0.7 }} />
                 <input
                     ref={inputRef}
                     type="text"
